61d6314b6946c0d7390331b950b1481b150a5caf
Mat-Kon <mat_kon@mail.ru>
2023-02-23 16:50:54 +0300

    feat: add hamburger menu for screen 380px

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ buttonCode.addEventListener('click', (event) => {
     const code = document.querySelector('.my-code');
     code.classList.toggle('active');
 })
+/*Hamburger menu*/
+
+ const burgerBtn = document.querySelector('.burger-button');
+ const menu = document.querySelector('.navbar');
+
+burgerBtn.addEventListener('click', function(){
+    burgerBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+});  
+
+menu.addEventListener('click', function(){
+    burgerBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+});
